6b3f9848c5df028ad672602f1162da72578513aa
tizavega <tizianovega220@gmail.com>
2026-08-17 13:02:06 -0300

    V5

---
+++ frmCaso5.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace cryVegaEjercitacionIG2
+{
+    partial class frmCaso5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUsuario = new Label();
+            txtUsuario = new TextBox();
+            lblClave = new Label();
+            txtClave = new TextBox();
+            btnIngresar = new Button();
+            SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(67, 54);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(47, 15);
+            lblUsuario.TabIndex = 0;
+            lblUsuario.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(169, 51);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(100, 23);
+            txtUsuario.TabIndex = 1;
+            // 
+            // lblClave
+            // 
+            lblClave.AutoSize = true;
+            lblClave.Location = new Point(67, 95);
+            lblClave.Name = "lblClave";
+            lblClave.Size = new Size(36, 15);
+            lblClave.TabIndex = 2;
+            lblClave.Text = "Clave";
+            // 
+            // txtClave
+            // 
+            txtClave.Location = new Point(169, 92);
+            txtClave.Name = "txtClave";
+            txtClave.Size = new Size(100, 23);
+            txtClave.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(124, 153);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(75, 23);
+            btnIngresar.TabIndex = 4;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // frmCaso5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(329, 241);
+            Controls.Add(btnIngresar);
+            Controls.Add(txtClave);
+            Controls.Add(lblClave);
+            Controls.Add(txtUsuario);
+            Controls.Add(lblUsuario);
+            Name = "frmCaso5";
+            Text = "LOGIN";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUsuario;
+        private TextBox txtUsuario;
+        private Label lblClave;
+        private TextBox txtClave;
+        private Button btnIngresar;
+    }
+}
---
+++ frmCaso5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cryVegaEjercitacionIG2
+{
+    public partial class frmCaso5 : Form
+    {
+        public frmCaso5()
+        {
+            InitializeComponent();
+        }
+    }
+}
