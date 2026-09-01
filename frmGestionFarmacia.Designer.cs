b9f33d0767e33283cdc0b6b28e9e859566dcc0a7
tizavega <tizianovega220@gmail.com>
2026-09-01 00:47:12 -0300

    gestion farmacia

---
+++ frmGestionFarmacia.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace cryVegaEjercitacionIG2
+{
+    partial class frmGestionFarmacia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btmBuscar = new Button();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            lblTipo = new Label();
+            txtMedicamento = new TextBox();
+            txtStock = new TextBox();
+            txtPrecio = new TextBox();
+            txtCodigo = new TextBox();
+            txtLaboratorio = new TextBox();
+            lblPrecio = new Label();
+            lblStock = new Label();
+            lblCodigo = new Label();
+            lblLaboratorio = new Label();
+            lblMedicamento = new Label();
+            SuspendLayout();
+            // 
+            // btmBuscar
+            // 
+            btmBuscar.Location = new Point(252, 410);
+            btmBuscar.Name = "btmBuscar";
+            btmBuscar.Size = new Size(75, 23);
+            btmBuscar.TabIndex = 48;
+            btmBuscar.Text = "Buscar";
+            btmBuscar.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(179, 264);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(442, 124);
+            listBox1.TabIndex = 47;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(360, 235);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 46;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(240, 235);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 45;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(326, 190);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 44;
+            // 
+            // lblTipo
+            // 
+            lblTipo.AutoSize = true;
+            lblTipo.Location = new Point(240, 193);
+            lblTipo.Name = "lblTipo";
+            lblTipo.Size = new Size(43, 15);
+            lblTipo.TabIndex = 43;
+            lblTipo.Text = "lblTipo";
+            // 
+            // txtMedicamento
+            // 
+            txtMedicamento.Location = new Point(327, 18);
+            txtMedicamento.Name = "txtMedicamento";
+            txtMedicamento.Size = new Size(100, 23);
+            txtMedicamento.TabIndex = 42;
+            // 
+            // txtStock
+            // 
+            txtStock.Location = new Point(326, 154);
+            txtStock.Name = "txtStock";
+            txtStock.Size = new Size(100, 23);
+            txtStock.TabIndex = 41;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(326, 120);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(100, 23);
+            txtPrecio.TabIndex = 40;
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Location = new Point(326, 86);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(100, 23);
+            txtCodigo.TabIndex = 39;
+            // 
+            // txtLaboratorio
+            // 
+            txtLaboratorio.Location = new Point(326, 52);
+            txtLaboratorio.Name = "txtLaboratorio";
+            txtLaboratorio.Size = new Size(100, 23);
+            txtLaboratorio.TabIndex = 38;
+            // 
+            // lblPrecio
+            // 
+            lblPrecio.AutoSize = true;
+            lblPrecio.Location = new Point(240, 123);
+            lblPrecio.Name = "lblPrecio";
+            lblPrecio.Size = new Size(40, 15);
+            lblPrecio.TabIndex = 37;
+            lblPrecio.Text = "Precio";
+            // 
+            // lblStock
+            // 
+            lblStock.AutoSize = true;
+            lblStock.Location = new Point(240, 157);
+            lblStock.Name = "lblStock";
+            lblStock.Size = new Size(36, 15);
+            lblStock.TabIndex = 36;
+            lblStock.Text = "Stock";
+            // 
+            // lblCodigo
+            // 
+            lblCodigo.AutoSize = true;
+            lblCodigo.Location = new Point(240, 89);
+            lblCodigo.Name = "lblCodigo";
+            lblCodigo.Size = new Size(46, 15);
+            lblCodigo.TabIndex = 35;
+            lblCodigo.Text = "Código";
+            // 
+            // lblLaboratorio
+            // 
+            lblLaboratorio.AutoSize = true;
+            lblLaboratorio.Location = new Point(240, 55);
+            lblLaboratorio.Name = "lblLaboratorio";
+            lblLaboratorio.Size = new Size(68, 15);
+            lblLaboratorio.TabIndex = 34;
+            lblLaboratorio.Text = "Laboratorio";
+            // 
+            // lblMedicamento
+            // 
+            lblMedicamento.AutoSize = true;
+            lblMedicamento.Location = new Point(240, 21);
+            lblMedicamento.Name = "lblMedicamento";
+            lblMedicamento.Size = new Size(81, 15);
+            lblMedicamento.TabIndex = 33;
+            lblMedicamento.Text = "Medicamento";
+            // 
+            // frmGestionFarmacia
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btmBuscar);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(lblTipo);
+            Controls.Add(txtMedicamento);
+            Controls.Add(txtStock);
+            Controls.Add(txtPrecio);
+            Controls.Add(txtCodigo);
+            Controls.Add(txtLaboratorio);
+            Controls.Add(lblPrecio);
+            Controls.Add(lblStock);
+            Controls.Add(lblCodigo);
+            Controls.Add(lblLaboratorio);
+            Controls.Add(lblMedicamento);
+            Name = "frmGestionFarmacia";
+            Text = "frmGestionFarmacia";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btmBuscar;
+        private ListBox listBox1;
+        private Button button2;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Label lblTipo;
+        private TextBox txtMedicamento;
+        private TextBox txtStock;
+        private TextBox txtPrecio;
+        private TextBox txtCodigo;
+        private TextBox txtLaboratorio;
+        private Label lblPrecio;
+        private Label lblStock;
+        private Label lblCodigo;
+        private Label lblLaboratorio;
+        private Label lblMedicamento;
+    }
+}
